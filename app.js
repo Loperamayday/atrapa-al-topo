37798e59806ab5e187ca7c593bf3396585e06e30
Loperamayday <123569796+Loperamayday@users.noreply.github.com>
2023-05-31 10:20:56 -0500

    1020

--- app.js
+++ app.js
@@ -7,8 +7,36 @@ let tiempoactual = tiempofaltante.textContent;
 
 function cuadradoazar {
   cuadrado.forEach (nombredeclase => { 
-    mombredeclase.classlist.remove("topo");
-  })
-}
+    nombredeclase.classlist.remove("topo");
+  });
 
-let posicionalazar = cuadrado [Math.floor (Math.random())]
+
+let posicionalazar = cuadrado [Math.floor (Math.random() * 9)];
+posicionalazar.classList.add("topo");
+
+
+posiciontopo= posicionalazar.id;
+  }
+
+  cuadrado.forEach((identificador) => {
+    if(identificador.id === posiciontopo) {
+      resultado = resultado + 1;
+      puntaje.textContent= resultado;
+      posicionalazar = null
+    }
+  }); 
+
+  function movertopo (){
+    tiempotopo = setInterval(cuadradoazar, 700)
+  }
+movertopo();
+
+function cuentaregresiva (){
+   tiempoactual --;
+   tiempofasltante.textContent = tiempoactual;
+   if (tiempoactual es === 0){
+   clearInterval(idtiempo);
+   clearInterval(tiempotopo);
+   alert ("se acabó el tiempo, tu puntaje es" + resultado + "topos atrapados")
+}
+}
